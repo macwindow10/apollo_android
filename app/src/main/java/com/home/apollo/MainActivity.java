7ee743cda268c1b88b6911848d394de19b529abc
macwindow10 <mac.window.10@gmail.com>
2025-11-22 17:24:52 +0500

    background image added to home fragment

--- app/src/main/java/com/home/apollo/MainActivity.java
+++ app/src/main/java/com/home/apollo/MainActivity.java
@@ -30,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
                 if (item.getItemId() == R.id.nav_home) {
                     selectedFragment = new HomeFragment();
-                } else if (item.getItemId() == R.id.nav_home) {
+                } else if (item.getItemId() == R.id.nav_log) {
                     selectedFragment = new LogFragment();
-                }else if (item.getItemId() == R.id.nav_home) {
+                }else if (item.getItemId() == R.id.nav_insights) {
                     selectedFragment = new InsightsFragment();
                 }
 
